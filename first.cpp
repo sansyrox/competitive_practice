dcf97ef48958ce695c8543f9bff3addeffca580e
Sanskar Jethi <sansyrox@gmail.com>
2018-10-10 02:18:21 +0530

    LL files

---
+++ first.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+#include "Node.cpp"
+
+Node* takeInput_Better() {
+  int data;
+  cin >> data;
+  Node *head = NULL;
+  Node *tail = NULL;
+  while(data != -1) {
+	Node *newNode = new Node(data);
+	if(head == NULL) {
+	  head = newNode;
+	  tail = newNode;
+	} 
+	else {
+	tail -> next = newNode;
+	tail = tail -> next;
+	}
+
+	cin>>data;
+
+ } 
+
+return head;
+
+}
+
+
+void print(Node *head) {
+  while(head != NULL) { 
+    cout<< head -> data << " ";
+    head = head -> next;
+ }
+
+}
+
+
